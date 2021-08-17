9b7775727eac31e38cc9dacdbe68f7415495089f
Li Shi <shili2048@gmail.com>
2021-08-17 16:23:02 +0800

    Update: malloc/free (not tested yet)

--- klib/include/klib-macros.h
+++ klib/include/klib-macros.h
@@ -1,6 +1,8 @@
 #ifndef KLIB_MACROS_H__
 #define KLIB_MACROS_H__
 
+#include <stddef.h>
+
 #define ROUNDUP(a, sz)      ((((uintptr_t)a) + (sz) - 1) & ~((sz) - 1))
 #define ROUNDDOWN(a, sz)    ((((uintptr_t)a)) & ~((sz) - 1))
 #define LENGTH(arr)         (sizeof(arr) / sizeof((arr)[0]))
