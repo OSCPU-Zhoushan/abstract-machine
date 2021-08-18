b50d6a85ee88620bb2a5fac838db6f863954606e
Li Shi <shili2048@gmail.com>
2021-08-18 16:24:31 +0800

    Update: timer update

--- am/src/mycpu/timer.c
+++ am/src/mycpu/timer.c
@@ -4,7 +4,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  asm volatile("csrr t0, mcycle; sd t0, %0" : :"m"(uptime->us));
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
