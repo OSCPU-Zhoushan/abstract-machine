d547719eb240321cb097cd060f26aebe4492baae
Li Shi <shili2048@gmail.com>
2021-10-06 14:37:55 +0800

    Timer: use mtime rather than mcycle to update time

--- am/src/mycpu/timer.c
+++ am/src/mycpu/timer.c
@@ -4,7 +4,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  asm volatile("csrr t0, mcycle; sd t0, %0" : :"m"(uptime->us));
+  uptime->us = *((uint64_t *)0x0200bff8);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
